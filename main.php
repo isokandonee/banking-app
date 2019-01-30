50513c32fbc42aa76b41050e8e16f62d9c8646b0
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2019-01-30 02:05:04 -0800

    my user app coming up nicely. added css to it

--- main.php
+++ main.php
@@ -1,6 +1,18 @@
 <?php
-$f = $_GET['fname'];
-$p = $_GET['pass'];
+$f = $_POST['fname'];
+$ph = $_POST['phone'];
+$pa = $_POST['pass'];
+$conn = mysqli_connect('localhost','root','','mitchel');
+$insert = mysqli_query($conn,"INSERT into my_users(name,phone,password) values('$f','$ph','$pa')");
+// $insert = mysqli_query($conn,'INSERT' 'INTO' `my-users`(`id`, `name`, `phone`, `password`) VALUES ([value-1],[$f],[$ph],[$pa]));
+if($insert){
+    echo "Registration successful!";
+}
+else {
+    echo "Registration not successful!".mysqli_error($conn);
 
-echo "Congratulations " .$f.", "."you have successfully logged in.";
+}
+$f = "";
+$ph = "";
+$pa = "";
 ?>
